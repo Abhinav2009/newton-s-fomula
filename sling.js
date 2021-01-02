00b4baf37f7db3af2dd43253253b1e1c832f933d
Abhinav2009 <72900427+Abhinav2009@users.noreply.github.com>
2021-01-02 17:54:00 +0530

    Add files via upload
    This is the newton's formula based please like it!!!!!!!!!!

---
+++ sling.js
@@ -0,0 +1,22 @@
+class Sling{
+constructor(bodyA, bodyB){
+var options = {
+bodyA: bodyA,
+pointB: pointB,
+stiffness: 1,
+angularstiffness: 1,
+length: 220
+};
+this.pointB = pointB
+this.pointX = bodyA.x;
+this.pointy = body.y - 250;
+this.sling = Constrait.create(options);
+AudioWorkletNode.a
+}
+
+
+
+
+
+
+}
